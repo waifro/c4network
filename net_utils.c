f524e9519e7cd2af9d92d9364b2a1e36667de6ee
waifro <waifro.bio@gmail.com>
2022-09-23 23:40:31 +0200

    minor fix

--- net_utils.c
+++ net_utils.c
@@ -96,6 +96,8 @@ int client_accept(int master_socket, struct sockaddr_in *addr) {
 
 int client_disconnect(cli_t *client_list, int index, net_lobby *lobby, struct sockaddr_in *addr, int *connected, int max_clients) {
 	
+	close(*socket);
+	
 	socklen_t addr_size = sizeof(struct sockaddr);
 	getpeername(client_list[index], (struct sockaddr*)addr, &addr_size);
 
